9765ab6bd4f58282553fd010312b001fcea3254c
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-07 23:13:04 +0300

    Status icons

---
+++ src/Components/Header/Svg/FollowSvg.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const FollowSvg = () => {
+    return (
+        <svg width="19" height="18" viewBox="0 0 12 14" fill="none">
+            <path 
+                d="M11.0667 13V11.6667C11.0667 10.9594 10.7857 10.2811 10.2856 9.78105C9.78551 9.28095 9.10723 9 8.39998 9H3.73332C3.02607 9 2.3478 9.28095 1.8477 9.78105C1.3476 10.2811 1.06665 10.9594 1.06665 11.6667V13"
+                stroke="white"
+                strokeWidth="1.33333"
+                strokeLinecap="round"
+                strokeLinejoin="round"/>
+            <path 
+                d="M6.06657 6.33333C7.53933 6.33333 8.73324 5.13943 8.73324 3.66667C8.73324 2.19391 7.53933 1 6.06657 1C4.59381 1 3.3999 2.19391 3.3999 3.66667C3.3999 5.13943 4.59381 6.33333 6.06657 6.33333Z"
+                stroke="white"
+                strokeWidth="1.33333"
+                strokeLinecap="round"
+                strokeLinejoin="round"/>
+        </svg>
+    )
+}
---
+++ src/Components/Header/Svg/FollowingSvg.js
@@ -0,0 +1,31 @@
+import React from 'react';
+
+export const FollowingSvg = () => {
+    return (
+        <svg width="19" height="18" viewBox="0 0 17 16" fill="none">
+            <g clipPath="url(#clip0)">
+                <path 
+                    d="M11.0667 14V12.6667C11.0667 11.9594 10.7857 11.2811 10.2856 10.781C9.78551 10.281 9.10723 10 8.39998 10H3.73332C3.02607 10 2.3478 10.281 1.8477 10.781C1.3476 11.2811 1.06665 11.9594 1.06665 12.6667V14"
+                    stroke="white"
+                    strokeWidth="1.33333"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+                <path 
+                    d="M6.06657 7.33333C7.53933 7.33333 8.73324 6.13943 8.73324 4.66667C8.73324 3.19391 7.53933 2 6.06657 2C4.59381 2 3.3999 3.19391 3.3999 4.66667C3.3999 6.13943 4.59381 7.33333 6.06657 7.33333Z"
+                    stroke="white"
+                    strokeWidth="1.33333"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+                <path 
+                    d="M11.7332 7.33333L13.0665 8.66667L15.7332 6"
+                    stroke="white"
+                    strokeWidth="1.33333"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                />
+            </g>
+        </svg> 
+    )
+}
