a0aa5c440005015af80d1ee026f1d1babaff0819
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-07 23:12:44 +0300

    Styles for the whole project

---
+++ src/styles/GlobalStyle.js
@@ -0,0 +1,86 @@
+import { createGlobalStyle, css } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    *, *:before, *:after {
+        box-sizing: border-box;
+        font-family: "Netflix Sans";
+        font-size: 16px;
+        outline: none;
+    }
+
+    html, body, div, span, applet, object, iframe,
+    h1, h2, h3, h4, h5, h6, p, blockquote, pre,
+    a, abbr, acronym, address, big, cite, code,
+    del, dfn, em, img, ins, kbd, q, s, samp,
+    small, strike, strong, sub, sup, tt, var,
+    b, u, i, center,
+    dl, dt, dd, ol, ul, li,
+    fieldset, form, label, legend,
+    table, caption, tbody, tfoot, thead, tr, th, td,
+    article, aside, canvas, details, embed, 
+    figure, figcaption, footer, header, hgroup, 
+    menu, nav, output, ruby, summary,
+    time, mark, audio, video {
+        margin: 0;
+        padding: 0;
+        border: 0;
+    }
+
+    a {
+        text-decoration: none;
+        color: rgba(84, 88, 247, 1);
+        transition: all 0.4s ease;
+
+        &:hover {
+            color: rgba(84, 88, 247, 0.8);
+        }
+    }
+
+    article, aside, details, figcaption, figure, 
+    footer, header, hgroup, menu, nav, section {
+        display: block;
+    }
+
+    a:active {
+        color: inherit;
+    }
+
+    body {
+        background-color: #191A1D;
+        color: #fff;
+
+        ${props => props.whiteTheme && css`
+            background-color: #fff;
+            color: #000;
+        `}
+    }
+
+    ol, ul {
+        list-style: none;
+    }
+
+    blockquote, q {
+        quotes: none;
+    }
+
+    blockquote:before, blockquote:after,
+    q:before, q:after {
+        content: '';
+        content: none;
+    }
+
+    table {
+        border-collapse: collapse;
+        border-spacing: 0;
+    }
+
+    textarea {
+        &::-webkit-scrollbar { 
+            width: 0; 
+        }
+    }
+
+    time {
+        font-size: inherit;
+    }
+`
