61db27618c46055db63e0a351901ba55ceb72d43
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-07 23:11:34 +0300

    Updates

--- static/js/main.8559068f.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpuser-profile"]=this["webpackJsonpuser-profile"]||[]).push([[0],{9:function(e,n,t){"use strict";t.r(n);var i=t(0),r=(t(2),t(3)),c=function(){return Object(i.jsx)("h1",{children:"Hi"})},o=function(e){e&&e instanceof Function&&t.e(3).then(t.bind(null,10)).then((function(n){var t=n.getCLS,i=n.getFID,r=n.getFCP,c=n.getLCP,o=n.getTTFB;t(e),i(e),r(e),c(e),o(e)}))};t.n(r).a.render(Object(i.jsx)(c,{}),document.getElementById("root")),o()}},[[9,1,2]]]);
-//# sourceMappingURL=main.8559068f.chunk.js.map
