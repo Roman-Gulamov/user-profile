3ca405b9802acf086471d9c94a2c56ab34a97e76
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-07 23:13:42 +0300

    Main file

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Profile from './Pages/Profile';
 import reportWebVitals from './reportWebVitals';
+import './assets/fonts/stylesheet.css';
 
 
 ReactDOM.render(<Profile />,document.getElementById('root'));
