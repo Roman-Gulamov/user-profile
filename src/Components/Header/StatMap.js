20bd67d190cf911758c4005b566896ebdc7d037c
Roman-Gulamov <scooby_roma_doo@mail.ru>
2020-11-07 23:16:08 +0300

    File for mapping the number of subscribers

---
+++ src/Components/Header/StatMap.js
@@ -0,0 +1,17 @@
+export const STAT_MAP = [
+    {
+        id: 1,
+        count: "142",
+        title: "posts"
+    },
+    {
+        id: 2,
+        count: "7.4m",
+        title: "followers"
+    },
+    {
+        id: 3,
+        count: "117",
+        title: "following"
+    }
+]
