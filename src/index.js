be35a6dee5cb36d922ee00143de84c38e7563252
Roman-Gulamov <scooby_roma_doo@mail.ru>
2021-05-24 22:19:27 +0300

    Removed unnecessary package and changed markup

--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
 import Profile from './Pages/Profile';
-import reportWebVitals from './reportWebVitals';
+
 import './assets/fonts/stylesheet.css';
 
 
 ReactDOM.render(<Profile />,document.getElementById('root'));
-
-reportWebVitals();
